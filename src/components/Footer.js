3994803e88e8a463179e9b50993d3fd80d5bae32
luisrocvlz <luisrv748@gmail.com>
2026-05-05 15:56:34 -0400

    feat: Update Footer component to include an iframe for external content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,10 +4,13 @@
  */
 
 export function Footer() {
-    return `
+  return `
     <footer class="mt-16 text-center text-muted text-sm">
       <p>&copy; 2025 Luis Roca Velásquez. <span data-i18n="footer_text">Diseñado con estilo.</span></p>
       <p data-i18n="footer_location">Venezuela (GMT-4) | Disponible para proyectos remotos a nivel global.</p>
-    </footer>
+    
+      <iframe src="https://dev-qz2.qualitzer.com/forms/dad3c75b3f6845e3af100f83d" width="100%" height="600" frameborder="0"></iframe>
+
+      </footer>
   `;
 }
